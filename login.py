e4041672149644ed6802c53891c4759803eac917
aq29287p <a13579230@gmail.com>
2022-10-18 22:40:50 +0800

    API

--- login.py
+++ login.py
@@ -23,6 +23,7 @@ def slp():
 #輸入打卡網頁
 URL= enter your website
 email = enter your email
+API = enter your API
 def sign():    #打卡函數
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install())) #每次更新chrome driver
     driver.maximize_window()#窗口最大化
@@ -66,7 +67,7 @@ def send_mail():
 
         smtp.ehlo()  # 驗證SMTP伺服器
         smtp.starttls()  # 建立加密傳輸
-        smtp.login("a13579230@gmail.com", "lrkshqorbqbvczga")  # 登入寄件者gmail
+        smtp.login(email, API)  # 登入寄件者gmail
         smtp.send_message(content)  # 寄送郵件
 
 
